a64df8768e33b4365de2ce4f7e9364c319c96059
jgartsu12 <jgartsu12@gmail.com>
2019-10-14 19:05:12 -0400

    populated courses data table

---
+++ capstone_project_populating_courses_table_data.sql
@@ -0,0 +1,54 @@
+USE sql_capstone_schema;
+
+INSERT INTO courses (courses_title)
+VALUES ("English 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("English 200");
+
+INSERT INTO courses (courses_title)
+VALUES ("Python 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("Python 200");
+
+INSERT INTO courses (courses_title)
+VALUES ("Calculus I");
+
+INSERT INTO courses (courses_title)
+VALUES ("Calculus II");
+
+INSERT INTO courses (courses_title)
+VALUES ("Calculus III");
+
+INSERT INTO courses (courses_title)
+VALUES ("Algorithms 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("JavaScript 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("Data Science 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("Fortran");
+
+INSERT INTO courses (courses_title)
+VALUES ("Meteorology 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("Advanced JS");
+
+INSERT INTO courses (courses_title)
+VALUES ("Advanced Algorithms");
+
+INSERT INTO courses (courses_title)
+VALUES ("Physics 101");
+
+INSERT INTO courses (courses_title)
+VALUES ("Physics 200");
+
+INSERT INTO courses (courses_title)
+VALUES ("Advanced Meteorology");
+
+
