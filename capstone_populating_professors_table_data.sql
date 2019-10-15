df7cc5737ca84109b35ca6efe4259c2e26636091
jgartsu12 <jgartsu12@gmail.com>
2019-10-15 10:05:25 -0400

    inserted professors data

---
+++ capstone_populating_professors_table_data.sql
@@ -0,0 +1,29 @@
+USE sql_capstone_schema;
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor X", "Maths Dept", 4, 6, null, 9);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Y", "Maths Dept", 5, 4, 8, 5);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Z", "English Dept", 1, 2, null, 1);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Pink", "Computer Science Dept", 11, 12, 15, 6);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Blue", "Computer Science Dept", 11, 13, 17, 3);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Yellow", "Maths Dept", 5, 6, 7, 2);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Dumbledore", "Meteorology Dept", 17, 15, 12, 3);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor Potter", "English Dept", 6, 14, null, 7);
+
+INSERT INTO professors (professors_name, professors_department, professors_courses_id, professors_courses_second_id, professors_courses_third_id, professors_grades_id)
+VALUES ("Professor G", "Computer Science", 12, 15, 11, 7);
+
