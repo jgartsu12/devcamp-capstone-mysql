919c89e430551763669c73b107e63917da45a4ef
jgartsu12 <jgartsu12@gmail.com>
2019-10-15 10:37:47 -0400

    used union to group students by courses enrolled in

---
+++ group_students_by_courses_enrolled_capstone.sql
@@ -0,0 +1,104 @@
+USE sql_capstone_schema;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='English 101'
+UNION
+SELECT students_name FROM students;
+
+USE sql_capstone_schema;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='English 200'
+UNION
+SELECT students_name FROM students;
+
+USE sql_capstone_schema;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Python 101'
+UNION
+SELECT students_name FROM students;
+
+USE sql_capstone_schema;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Python 200'
+UNION
+SELECT students_name FROM students;
+
+USE sql_capstone_schema;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Calculus I'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Calculus II'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Calculus III'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Algorithms 101'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Javascript 101'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Data Science 101'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Fortran'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Meteorology 101'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Advanced JS'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Advanced Algorithims'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Physics 101'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Physics 200'
+UNION
+SELECT students_name FROM students;
+
+SELECT courses_title FROM courses
+WHERE courses_title ='Advanced Meteorology'
+UNION
+SELECT students_name FROM students;
+
+
+
+
+
+
+
+
+
+
