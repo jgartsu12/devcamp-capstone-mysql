b3a4b0ec7bd22bf4b4a2fbd970afad7c0037bfff
jgartsu12 <jgartsu12@gmail.com>
2019-10-15 10:07:14 -0400

    added student data table

---
+++ insert_student_data_capstone.sql
@@ -0,0 +1,54 @@
+USE sql_capstone_schema;
+
+INSERT INTO students (students_name, students_major, students_grades_id, students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Matt", "Bio", 3, 1, 3, 5, 8, null);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Jake", "Bio", 1, 1, 3, 5, 8, 11);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("John", "Bio", 1, 6, 5, 4, 17, 16);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Kerry", "Bio", 9, 4, 7, 8, 10, 11);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Sam", "Bio", 6, 9, 11, 12, 3, 17);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Phil", "Bio", 7, 4, 5, 6, 14, 16);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Jay", "Computer Science", 4, 10, 12, 14, null, null);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Lukas", "Computer Science", 1, 5, 6, 7, 12, 17);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Lilly", "Computer Science", 2, 7, 9, 3, 2, 9);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Corrine", "Computer Science", 2, 3, 4, 5, 7, 9);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Jilly", "Computer Science", 9, 1, 3, 5, 7, null);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Penelope", "Computer Science", 4, 17, 16, 11, 5, 9);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Jim", "Computer Science", 4, 17, 16, 11, 5, 9);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Joe", "Computer Science", 4, 17, 15, 11, null, null);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Happy", "Computer Science", 4, 10, 12, 11, 5, 9);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Bill", "English", 4, 2, 3, 11, 5, 9);
+
+INSERT INTO students (students_name, students_major, students_grades_id,students_courses_first_id, students_courses_second_id, students_courses_third_id, students_courses_fourth_id, students_courses_fifth_id)
+VALUES("Harry Potter", "English", 4, 17, 16, 11, 5, null);
+
+
