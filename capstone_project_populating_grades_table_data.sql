0405325af7a6dd8e3360ebdcf6a765ea526a21ab
jgartsu12 <jgartsu12@gmail.com>
2019-10-15 09:09:51 -0400

    added grades data table

---
+++ capstone_project_populating_grades_table_data.sql
@@ -0,0 +1,37 @@
+USE sql_capstone_schema;
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("A+", "A+", 1, 4);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("B+", "B+", 2, 5);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("C+", "C+", 3, 4);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("A", "A", 4, 7);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("B", "B", 5,12);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("C", "C", 1, 12);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("D", "D", 7, 7);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("F", "F", 8, 6);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("I", "I", 9, 5);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("A-", "A-", 8, 11);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("B-", "B-", 11, 9);
+
+INSERT INTO grades (grades_average, grades_top, grades_students_id, grades_professors_id)
+VALUES ("C-", "C-", 12, 9);
